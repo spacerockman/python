ae25abe49491c4afa311e5457fc56d6fc3b23462
spacerockman <xujintao.irl@gmail.com>
2022-05-21 18:40:48 +0900

    style: add comments

--- marvel_comic_download/convert_mutiple_jpgs_to_pdf_at_current_path.py
+++ marvel_comic_download/convert_mutiple_jpgs_to_pdf_at_current_path.py
@@ -3,6 +3,7 @@ import os
 import sys
 import natsort
 
+# get the path and current directory name
 dir_name = sys.path[0]
 curr_directory_name = dir_name.split("/")[-1]
 
@@ -14,14 +15,14 @@ for i in jpg_files:
     if i.endswith('.jpg'):
         files_list.append(i)
 
-# sored files by name
+# sorted files by name
 file_list = natsort.natsorted(files_list)
 
 # show the order
 for i in file_list:
     print(i)
 
-# imagelist is the list with all image filenames
+# file_list is the list with all image filenames
 for image in file_list:
     pdf.add_page()
     pdf.image(f"{dir_name}/{image}", w=190)
